b1ff37e2979ba04d61834502083f0c9c6a23ce48
adalouslife <giannifrancesco39@gmail.com>
2025-09-19 11:40:22 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-set -e
-
-echo "[INFO] Starting anonymizer worker..."
-# Ensure uploads dir exists
-mkdir -p /app/uploads
-
-# Start RunPod handler
-python3 handler.py
+#!/usr/bin/env bash
+set -euo pipefail
+python handler.py
